82738dcf7e169a5f89b018b78685deadd7959595
Lgrebol <lgrebol08@boscdelacoma.cat>
2025-01-27 17:48:40 +0100

    IMPLEMENTED TEST should fetch players

--- src/app/FRONTEND/players/players.component.ts
+++ src/app/FRONTEND/players/players.component.ts
@@ -20,7 +20,20 @@ export class PlayersComponent implements OnInit {
   positions = ['Goalkeeper', 'Defender', 'Midfielder', 'Forward']; 
 
   constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.fetchPlayers();
+  }
 
 
-  
+  // Obtenir jugadors del backend
+  fetchPlayers() {
+    this.http.get<any[]>('http://localhost:3000/api/v1/players').subscribe(
+      (data) => {
+        this.players = data;
+      },
+      (error) => {
+        console.error('Error carregant els jugadors:', error);
+      }
+    );
+  }  
 }
