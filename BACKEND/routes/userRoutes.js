ec112d4082574bdd5b54c98d94023b8b3dab2154
Lgrebol <lgrebol08@boscdelacoma.cat>
2025-02-26 20:00:41 +0100

    Game fixed

--- BACKEND/routes/userRoutes.js
+++ BACKEND/routes/userRoutes.js
@@ -1,9 +1,7 @@
 import express from "express";
 import { registerUsers, loginUsers, getUsers } from "../controllers/userController.js";
-import { authMiddleware } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
-router.use(authMiddleware);
 router.post("/register", registerUsers);
 router.post("/login", loginUsers);
 router.get("/", getUsers);
