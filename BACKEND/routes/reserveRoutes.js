739f72781b6b78f3c9017341b398310180764ccf
Lgrebol <lgrebol08@boscdelacoma.cat>
2025-02-26 18:19:17 +0100

    a

--- BACKEND/routes/reserveRoutes.js
+++ BACKEND/routes/reserveRoutes.js
@@ -1,8 +1,7 @@
 import express from "express";
 import { getReservedPlayers } from "../controllers/reserveController.js";
-import { authMiddleware } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
-router.get("/:userId", authMiddleware, getReservedPlayers);
+router.get("/:userId", getReservedPlayers);
 
 export default router;
