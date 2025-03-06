910d246904397cb3b40cbb9991fc0643f4b16cda
Lgrebol <lgrebol08@boscdelacoma.cat>
2025-03-06 15:13:06 +0100

    Database created + port microsoft sql server

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const dbConfig = {
   server: process.env.DB_SERVER,
+  port: parseInt(process.env.DB_PORT, 10),
   database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
